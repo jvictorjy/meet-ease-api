855bdc8915fa31d63092218bfbc53462dfdb96fc
Victor Ribeiro <jvictorjy@gmail.com>
2025-04-08 23:24:35 -0300

    feat: add environment validation and integrate with main application bootstrap

---
+++ src/env.validation.ts
@@ -0,0 +1,77 @@
+import {
+  plainToInstance,
+  Transform,
+  TransformFnParams,
+} from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, validateSync } from 'class-validator';
+
+const transform = {
+  toInt: ({ value }: TransformFnParams) => parseInt(value),
+};
+
+export class EnvironmentVariables {
+  @IsNotEmpty()
+  @IsString()
+  NODE_ENV: string;
+
+  @IsNotEmpty()
+  @Transform(transform.toInt)
+  @IsInt()
+  PORT: number;
+
+  @IsNotEmpty()
+  @IsString()
+  JWT_SECRET: string;
+
+  @IsNotEmpty()
+  @IsString()
+  JWT_PRIVATE_KEY: string;
+
+  @IsNotEmpty()
+  @IsString()
+  JWT_PUBLIC_KEY: string;
+
+  @IsNotEmpty()
+  @IsString()
+  DB_DIALECT: string;
+
+  @IsNotEmpty()
+  @IsString()
+  DB_HOST: string;
+
+  @IsNotEmpty()
+  @Transform(transform.toInt)
+  @IsInt()
+  DB_PORT: number;
+
+  @IsNotEmpty()
+  @IsString()
+  DB_USER: string;
+
+  @IsNotEmpty()
+  @IsString()
+  DB_PASSWORD: string;
+
+  @IsNotEmpty()
+  @IsString()
+  DB_DATABASE: string;
+
+  @IsNotEmpty()
+  @IsString()
+  DB_LOGGING: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: false,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from './env.validation';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService =
+    app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Delivery API')
+    .setDescription('API for calculating delivery fees with margin')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
