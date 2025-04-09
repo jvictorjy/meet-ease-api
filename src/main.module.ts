6d67c3bfb4f494f36b7704cd6df38368a5536af0
Victor Ribeiro <jvictorjy@gmail.com>
2025-04-09 01:51:35 -0300

    feat: refactor application structure and introduce core response handling

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { validate } from './env.validation';
+import { validate } from '@core/@shared/infrastructure/config/env.validation';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, validate })],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class MainModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MainModule } from './main.module';
 import { ConfigService } from '@nestjs/config';
-import { EnvironmentVariables } from './env.validation';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { EnvironmentVariables } from '@core/@shared/infrastructure/config/env.validation';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MainModule);
 
   const configService =
     app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
 
   const config = new DocumentBuilder()
-    .setTitle('Delivery API')
-    .setDescription('API for calculating delivery fees with margin')
+    .setTitle('Meet Ease API')
+    .setDescription('API to make room reservations')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, document);
 
-  await app.listen(configService.get('PORT') ?? 3000);
+  await app.listen(configService.get('PORT') ?? 3000).then(() => {
+    console.log(
+      `🚀 meet-ease-api is running in http://localhost:${configService.get(
+        'PORT',
+      )}`,
+    );
+  });
 }
 bootstrap();
